088bb4d6ed918bce1934450f82d550cd2eaf1fec
Takács Marcell <tmarcell20012001@gmail.com>
2022-03-28 12:18:13 +0200

    hapi

--- hirdetesapi/models/hirdetes.js
+++ hirdetesapi/models/hirdetes.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+function arFtvalidator(value){
+    return value % 1000 === 0
+}
+
 const hirdetesSchema = new Schema({
     _id: Number,
     kategoria :{
         type : Number,
-        default : 1
+        default : 1,
+        ref: 'Kategoria'
     },
     cim: {
         type: String,
@@ -25,7 +30,8 @@ const hirdetesSchema = new Schema({
     serulesmentes : Boolean,
     arFt: {
         type : Number,
-        required : true
+        required : true,
+        validate : [arFtvalidator, "Az ár nem oszthtó  1000-el"]
     },
     kepUrl:{
         type : String,
--- hirdetesapi/routes/hahu.js
+++ hirdetesapi/routes/hahu.js
@@ -1,7 +1,9 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 
 const Hirdetes = require('../models/hirdetes')
+const Kategoria = require('../models/kategoria')
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
@@ -14,13 +16,44 @@ router.post('/', function(req, res, next) {
   const arFt = req.body.arFt;
   const kepUrl = req.body.kepUrl;
 
+
   const hirdetes = new Hirdetes({_id,kategoria,cim,leiras,hirdetesDatum,serulesmentes,arFt,kepUrl});
   hirdetes
-  .save()
+  .save(function(err) {
+    if(err){
+      return res.status(400).json({success : false, message: err.message})
+    }
+    res.status(200).json({success: true})
+  })
   .then(res.json({
-      status:'created'
+      'status':'created'
   }))
   .catch(err => console.log(err))
 });
 
+router.get('/:mezo', function(req, res, next) {
+  const mezo = req.params.mezo;
+  Hirdetes.find()
+  .populate("kategoria", "-_id")
+  .sort({[mezo] : 1})
+  .then(hirdetesek => {
+    res.json({hirdetesek})
+  })
+  .catch(err => console.log(err))
+})
+
+router.delete('/:id', function(req, res, next) {
+  const id = req.params.id;
+
+  Hirdetes.findById(id).then(doc =>{
+    if(doc === null){
+      return res.json({
+        error:"A hirdetes ilyen azonosítróval nem létezik"
+      })
+    }
+    Hirdetes.findByIdAndDelete(id)
+  .then(res.json({"status" : `A hirdetes ${id} azonosítója törölve`}))
+  .catch(err => console.log(err))
+  })
+})
 module.exports = router;
